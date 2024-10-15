5a38edbb11ed4516afc29c65308e632450bb9292
D3FAU4T <d3fau4t@gmail.com>
2024-10-15 22:33:22 +0530

    Added build script for platform-specific builds

---
+++ build.py
@@ -0,0 +1,44 @@
+import color
+import platform
+import subprocess
+
+def build():
+    os_type = platform.system()
+    
+    if os_type == 'Windows':
+        command = [
+            "pyinstaller",
+            "--onefile",
+            "--name", "Comparer",
+            "--icon", "static/icon.ico",
+            "--add-data", "templates;templates",
+            "--add-data", "static;static",
+            "--add-data", "color.py;.",
+            "main.py"
+        ]
+    
+    elif os_type == 'Linux':
+        command = [
+            "pyinstaller",
+            "--onefile",
+            "--name", "Comparer",
+            "--add-data=templates:templates",
+            "--add-data=static:static",
+            "--add-data=color.py:.",
+            "main.py"
+        ]
+        
+    else:
+        print(color.red('× Unsupported OS'))
+        return
+    
+    try:
+        subprocess.run(command)
+        print(color.green('✓ Build successful for platform:'), color.yellow(os_type))
+    except subprocess.CalledProcessError as err:
+        print(color.red('× Build failed for platform:'), color.yellow(os_type))
+        print(color.red('× Error:'), err)
+        
+
+if __name__ == '__main__':
+    build()
